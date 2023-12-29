4de9929701d9ce5eb25a012195e2c4b93bf5e0fc
Farhan <farfnd@yahoo.com>
2023-12-29 14:27:56 +0800

    feat(model): add education model & resources

---
+++ database/factories/EducationFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Education>
+ */
+class EducationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $candidate = \App\Models\Candidate::inRandomOrder()->first();
+        if (!$candidate) {
+            $candidate = \App\Models\Candidate::factory()->create();
+        }
+
+        return [
+            'candidate_id' => $candidate->id,
+            'institution_name' => $this->faker->company,
+            'major' => $this->faker->jobTitle,
+            'start_year' => $this->faker->year,
+            'end_year' => $this->faker->year,
+            'until_now' => $this->faker->boolean,
+            'gpa' => $this->faker->randomFloat(2, 1, 4),
+            'flag' => $this->faker->randomElement(['flag1', 'flag2', 'flag3']),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CandidateSeeder::class,
+            EducationSeeder::class,
         ]);
     }
 }
