ad712a39893bfc7d83b1bcad9db0232bf035351f
Farhan <farfnd@yahoo.com>
2023-12-29 14:44:56 +0800

    refactor(database): change 'educations' table name to 'education'

--- database/migrations/2023_12_29_055132_create_education_table.php
+++ database/migrations/2023_12_29_055132_create_education_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('educations', function (Blueprint $table) {
+        Schema::create('education', function (Blueprint $table) {
             $table->id();
             $table->foreignId('candidate_id')->constrained();
             $table->string('institution_name');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('educations');
+        Schema::dropIfExists('education');
     }
 };
