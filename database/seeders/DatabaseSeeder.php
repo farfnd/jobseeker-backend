66373969af532f5f8f50e0170fc5d368cc7d81dc
Farhan <farfnd@yahoo.com>
2023-12-29 14:43:31 +0800

    feat(database): create province model, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            ProvinceSeeder::class,
+
             CandidateSeeder::class,
             EducationSeeder::class,
             ExperienceSeeder::class,
